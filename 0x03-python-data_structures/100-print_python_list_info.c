c188b7e60ff6b0d2a8d13b797aeed2661ed985d9
Neville256 <nsoundy796@gmail.com>
2023-06-10 06:23:28 -0700

    1-element_at.py

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,25 +1,25 @@
 #include <Python.h>
 
 /**
- * print_python_list_info - Python lists prints basic
+ * print_python_list_info - Python list basic information printer
  * @p: A PyObject list.
  */
 void print_python_list_info(PyObject *p)
 {
-	int size, alloc, k;
-	PyObject *obj;
+    int size, alloc, k;
+    PyObject *obj;
 
-	size = Py_SIZE(p);
-	alloc = ((PyListObject *)p)->allocated;
+    size = PyList_Size(p);  // Use PyList_Size() instead of Py_SIZE() to get the size of the list
+    alloc = ((PyListObject *)p)->allocated;
 
-	printf("[*] size of python list = %d\n", size);
-	printf("[*] Allocated = %d\n", alloc);
+    printf("[*] Size of the Python list = %d\n", size);  // Updated print statement
+    printf("[*] Allocated = %d\n", alloc);
 
-	for(k = 0; k < size; k++)
-	{
-		printf("Element %d: ", k);
+    for (k = 0; k < size; k++)
+    {
+        printf("Element %d: ", k);
 
-		obj = PyList_GetItem(p, k);
-		pintf("%s\n", Py_TYPE(obj)->tp_name);
-	}
+        obj = PyList_GetItem(p, k);
+        printf("%s\n", Py_TYPE(obj)->tp_name);  // Corrected the typo 'pintf' to 'printf'
+    }
 }
