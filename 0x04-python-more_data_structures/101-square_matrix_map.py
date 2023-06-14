db8da9b02678520dffb8f4eeb4def542e002db1a
Neville256 <nsoundy796@gmail.com>
2023-06-14 05:30:50 -0700

    101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,12 +1,3 @@
-#!/usr/bin/python
-def complex_delete(a_dictionary, value):
-    keys_to_delete = []
-
-    for key, val in a_dictionary.items():
-        if val == value:
-            keys_to_delete.append(key)
-
-    for key in keys_to_delete:
-        del a_dictionary[key]
-
-    return a_dictionary
+#!/usr/bin/python3
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
