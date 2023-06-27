6fd8855008c2240396812d00f434c08209241840
Neville256 <nsoundy796@gmail.com>
2023-06-27 07:39:30 -0700

    0-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,19 +6,17 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize new square.
+        """Initialize a new square.
 
         Args:
-         size(int): size of new square.
-    """
+            size (int): Size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    slef.__size = size
-
-
-def area(self):
-    """Return current area of square."""
-    return(self.__size * self.__size)
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size * self.__size
