d790939742173c955bf77c20fb9e1a7e285746e0
Neville256 <nsoundy796@gmail.com>
2023-06-27 07:30:20 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize new square.
+
+        Args:
+         size(int): size of new square.
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    slef.__size = size
+
+
+def area(self):
+    """Return current area of square."""
+    return(self.__size * self.__size)
