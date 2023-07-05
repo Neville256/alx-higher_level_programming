64f603adedfa9f823a3dd29b5dd6e2c9a2f015d4
Neville256 <nsoundy796@gmail.com>
2023-07-05 07:40:33 -0700

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,16 +3,25 @@
 """Defines an integer addition function."""
 
 
-def add_integer(a, b=98):
+def add_integer(a=None, b=98):
     """Return the integer addition of a and b.
 
     Float arguments are typecasted to ints before addition.
 
     Raises:
         TypeError: If either a or b is non-integer and non-float.
+        ValueError: If a or b cannot be converted to integers.
     """
+    if a is None:
+        raise TypeError(
+            "add_integer() missing 1 required positional argument: 'a'"
+        )
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or float")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer or float")
-    return int(a) + int(b)
+
+    try:
+        return int(a) + int(b)
+    except ValueError:
+        raise ValueError("a or b cannot be converted to integers")
