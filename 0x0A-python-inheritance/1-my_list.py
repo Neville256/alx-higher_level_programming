280708590deb670693dcd1d2bfd29cfca91d2032
Neville256 <nsoundy796@gmail.com>
2023-07-10 07:27:03 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+===========================
+Module with class MyList
+===========================
+"""
+
+
+class MyList(list):
+    """method print_sorted with class"""
+    pass
+
+    def print_sorted(self):
+        """sorted list of a method"""
+
+        print(sorted(list(self)))
