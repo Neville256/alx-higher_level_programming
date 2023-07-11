cf4a6b433a05ee3d3b6a405d3ded600039ff5cf6
Neville256 <nsoundy796@gmail.com>
2023-07-11 13:36:04 -0700

    0-main.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-
+"""
+3-to_json_string module
+"""
 import json
 
 
 def to_json_string(my_obj):
-    """function that returns the JSON representation of an object (string)"""
-    return json.dumps(my_obj)i
+    """
+    to_json_string - returns the JSON representation of an object (string):
+    Args:
+        my_obj: string to represent
+    Return: json representation
+    """
+    return json.dumps(my_obj)
