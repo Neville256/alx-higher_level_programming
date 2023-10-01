82a89d1faf55b80508ffa419ffc000d1dc7e1b6d
Neville256 <nsoundy796@gmail.com>
2023-10-01 01:23:14 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sX GET $1
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
+response=$(curl -sI $1 | grep -i "Content-Length" | awk '{print $2}')
+if [ -n "$response" ]; then
+    echo "Content-Length: $response bytes"
+else
+    echo "Content-Length not found in the response headers."
+fi
