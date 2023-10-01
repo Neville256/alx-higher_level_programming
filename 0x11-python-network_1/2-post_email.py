854aae2731a5dc82da3eec27214060b6e2308af3
Neville256 <nsoundy796@gmail.com>
2023-10-01 00:54:42 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,6 @@ from sys import argv
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
-
 if __name__ == "__main__":
     url = argv[1]
     value = {"email": argv[2]}
@@ -16,4 +15,4 @@ if __name__ == "__main__":
     req = Request(url, data)
 
     with urlopen(req) as response:
-        print(response.read().decode("utf-8", "replace")
+        print(response.read().decode("utf-8", "replace"))
