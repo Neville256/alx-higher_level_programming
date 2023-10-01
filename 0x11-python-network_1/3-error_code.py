dda8c29f6dc36a11e1c89c851c319577fbdf6964
Neville256 <nsoundy796@gmail.com>
2023-10-01 00:58:13 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,13 +10,15 @@ from sys import argv
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError
 
-
 if __name__ == "__main__":
-    url = argv[1]
-    req = Request(url)
+    if len(argv) != 2:
+        print("Usage: ./3-error_code.py <URL>")
+    else:
+        url = argv[1]
+        req = Request(url)
 
-    try:
-        with urlopen(req) as response:
-            print(response.read().decode("ascii"))
-    except HTTPError as e:
-        print("Error code: {}".format(e.code))
+        try:
+            with urlopen(req) as response:
+                print(response.read().decode("utf-8"))
+        except HTTPError as e:
+            print(f"Error: {e.code} - {e.reason}")
