1d4a92ed3cda92e558cb00a0190ddd7b93969756
Neville256 <nsoundy796@gmail.com>
2023-10-01 01:52:08 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,26 +1,13 @@
 #!/usr/bin/python3
-"""
-Script that takes in a URL, sends a request to the URL and displays the
-value of the variable X-Request-Id in the response header.
+"""Takes in a URL, sends a request to the URL
+and displays the value of the variable X-Request-Id
+in the response header"""
 
-Usage: ./5-hbtn_header.py <URL>
-"""
-import requests
-from sys import argv
-
-
-if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: ./5-hbtn_header.py <URL>")
-    else:
-        url = argv[1]
-        try:
-            req = requests.get(url)
-            req.raise_for_status()  # Raise an exception if the response status is not 2xx
-            x_request_id = req.headers.get("X-Request-Id")
-            if x_request_id:
-                print(x_request_id)
-            else:
-                print("X-Request-Id header not found in the response.")
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
+if __name__ == '__main__':
+    import sys
+    import requests
+    try:
+        response = requests.get(sys.argv[1])
+        print(response.headers.get('X-Request-Id'))
+    except Exception:
+        pass
