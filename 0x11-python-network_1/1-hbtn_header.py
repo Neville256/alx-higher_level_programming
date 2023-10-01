8e740ea91e2606d5cf85deb376885423b64d5ae7
Neville256 <nsoundy796@gmail.com>
2023-10-01 01:46:12 -0700

    8

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-"""A script that takes in a URL, sends a request and display the value
-in the X-Request-Id variable found in the header"""
+"""
+Script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response.
 
-if __name__ == '__main__':
-    import sys
-    import urllib.request
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        url_res = response.info()
-        print(url_res['X-Request-Id']
+Usage: ./1-hbtn_header.py <URL>
+"""
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: ./1-hbtn_header.py <URL>")
+    else:
+        url = argv[1]
+        req = Request(url)
+
+        try:
+            with urlopen(req) as response:
+                x_request_id = response.headers.get("X-Request-Id")
+                if x_request_id:
+                    print(x_request_id)
+                else:
+                    print("X-Request-Id header not found in the response.")
+        except URLError as e:
+            print(f"Error: {e}")
+        except HTTPError as e:
+            print(f"HTTP Error: {e}")
