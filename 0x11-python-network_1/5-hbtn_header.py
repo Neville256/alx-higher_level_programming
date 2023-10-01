eb02cfb20cbefdc313025abc4c5edf147aa9a7f6
Neville256 <nsoundy796@gmail.com>
2023-10-01 01:03:49 -0700

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,7 +10,17 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = argv[1]
-    req = requests.get(url)
-
-    print(req.headers.get("X-Request-Id"))
+    if len(argv) != 2:
+        print("Usage: ./5-hbtn_header.py <URL>")
+    else:
+        url = argv[1]
+        try:
+            req = requests.get(url)
+            req.raise_for_status()  # Raise an exception if the response status is not 2xx
+            x_request_id = req.headers.get("X-Request-Id")
+            if x_request_id:
+                print(x_request_id)
+            else:
+                print("X-Request-Id header not found in the response.")
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
